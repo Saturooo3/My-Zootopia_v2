f727cd0fd10a1a641cb8c4d9c7b8d70eba38cea3
Saturooo3 <yusufbulukgiray@gmail.com>
2025-03-04 18:00:47 +0100

    gets data from live api

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,52 +1,47 @@
+import requests
 import json
 
+API_KEY = "V91oA1MxaTuaFB9nzObLhg==v7INuvzcMhLn6D82"
 
-def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
 
-def serialize_animal(animal):
-    """
-    takes single element of list with data of animal
-    and return a str with the data organized for a html file
-    """
+def load_data(name):
+    api_url = f'https://api.api-ninjas.com/v1/animals?name={name}'
+    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
+    if response.status_code == requests.codes.ok:
+        return response.json()
+    else:
+        print("Error:", response.status_code, response.text)
+
+
+
+def serialize_data(animal):
     output = ""
     output += "<li class='cards__item'>"
 
-    if animal["name"]:
-        name = animal["name"]
-        output += f"\n<div class='card__title'>{name}</div>"
-
+    output += f"\n<div class='card__title'>{animal['name']}</div>"
     output += "\n<p class ='card__text'>"
-
-    if animal["characteristics"]["diet"]:
-        diet = animal["characteristics"]["diet"]
-        output += f"\n<strong>Diet:</strong> {diet}<br/>\n"
-
-    if animal["locations"][0]:
-        locations = animal["locations"][0]
-        output += f"<strong>Location</strong>: {locations}<br/>\n"
-
-    if "type" in animal["characteristics"].keys():
-        animal_type = animal["characteristics"]["type"]
-        output += f"<strong>Type</strong>: {animal_type}<br/>\n"
+    output += f"\n<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n"
+    output += "<strong>Locations:</strong><br/>"
+    for location in animal["locations"]:
+        output += f"\n\t{location}<br/>"
+    if "grop" in animal["characteristics"].keys():
+        output += f"\n<strong>Group</strong>: {animal["characteristics"]["group"]}<br/>\n"
 
     output += "</p>\n</li>\n"
-
     return output
 
-
-def transform_data_to_str(animals_data: list) -> str:
+def listed_data(animal, animals_data: list) -> str:
     """
     takes a list of animals data and return a str with name, diet,
     locations and type of animal for html
     """
     output = ""
-
+    if len(animals_data) == 0:
+        output += f"<h2>The animaal '{animal}' doesn't exist.</h2>"
     for animal in animals_data:
-        output += serialize_animal(animal)
+        output += serialize_data(animal)
     return output
+
 
 
 def implement_animals_info(animals_str: str) -> str:
@@ -69,11 +64,12 @@ def write_data(script: str) -> None:
 
 
 def main():
-    animals_data = load_data("animals_data.json")
-    serialized_animal_str = transform_data_to_str(animals_data)
-    script = implement_animals_info(serialized_animal_str)
+    name= input("Enter a name of an animal: ")
+    animals_data = load_data(name)
+    serialized_data = listed_data(name, animals_data)
+    script = implement_animals_info(serialized_data)
     write_data(script)
-
+    print("Website was successfully generated to the file animals.html")
 
 if __name__ == "__main__":
     main()
