1a79a452ca29448542eabc386afdce9359bf5620
Saturooo3 <yusufbulukgiray@gmail.com>
2025-03-04 18:09:28 +0100

    new project architecture

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,47 +1,4 @@
-import requests
-import json
-
-API_KEY = "V91oA1MxaTuaFB9nzObLhg==v7INuvzcMhLn6D82"
-
-
-def load_data(name):
-    api_url = f'https://api.api-ninjas.com/v1/animals?name={name}'
-    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
-    if response.status_code == requests.codes.ok:
-        return response.json()
-    else:
-        print("Error:", response.status_code, response.text)
-
-
-
-def serialize_data(animal):
-    output = ""
-    output += "<li class='cards__item'>"
-
-    output += f"\n<div class='card__title'>{animal['name']}</div>"
-    output += "\n<p class ='card__text'>"
-    output += f"\n<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n"
-    output += "<strong>Locations:</strong><br/>"
-    for location in animal["locations"]:
-        output += f"\n\t{location}<br/>"
-    if "grop" in animal["characteristics"].keys():
-        output += f"\n<strong>Group</strong>: {animal["characteristics"]["group"]}<br/>\n"
-
-    output += "</p>\n</li>\n"
-    return output
-
-def listed_data(animal, animals_data: list) -> str:
-    """
-    takes a list of animals data and return a str with name, diet,
-    locations and type of animal for html
-    """
-    output = ""
-    if len(animals_data) == 0:
-        output += f"<h2>The animaal '{animal}' doesn't exist.</h2>"
-    for animal in animals_data:
-        output += serialize_data(animal)
-    return output
-
+import data_fetcher
 
 
 def implement_animals_info(animals_str: str) -> str:
@@ -64,9 +21,8 @@ def write_data(script: str) -> None:
 
 
 def main():
-    name= input("Enter a name of an animal: ")
-    animals_data = load_data(name)
-    serialized_data = listed_data(name, animals_data)
+    animal_name= input("Enter a name of an animal: ")
+    serialized_data = data_fetcher.fetch_data(animal_name)
     script = implement_animals_info(serialized_data)
     write_data(script)
     print("Website was successfully generated to the file animals.html")
--- data_fetcher.py
+++ data_fetcher.py
@@ -1,18 +1,62 @@
+import requests
+
+
+API_KEY = "V91oA1MxaTuaFB9nzObLhg==v7INuvzcMhLn6D82"
+
+
+def load_data(name):
+    api_url = f'https://api.api-ninjas.com/v1/animals?name={name}'
+    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
+    if response.status_code == requests.codes.ok:
+        return response.json()
+    else:
+        print("Error:", response.status_code, response.text)
+
+
+def serialize_data(animal):
+    output = ""
+    output += "<li class='cards__item'>"
+
+    output += f"\n<div class='card__title'>{animal['name']}</div>"
+    output += "\n<p class ='card__text'>"
+    output += f"\n<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n"
+    output += "<strong>Locations:</strong><br/>"
+    for location in animal["locations"]:
+        output += f"\n\t{location}<br/>"
+    if "grop" in animal["characteristics"].keys():
+        output += f"\n<strong>Group</strong>: {animal["characteristics"]["group"]}<br/>\n"
+
+    output += "</p>\n</li>\n"
+    return output
+
+def listed_data(animal, animals_data: list) -> str:
+    """
+    takes a list of animals data and return a str with name, diet,
+    locations and type of animal for html
+    """
+    output = ""
+    if len(animals_data) == 0:
+        output += f"<h2>The animal '{animal}' doesn't exist.</h2>"
+    for animal in animals_data:
+        output += serialize_data(animal)
+    return output
+
 def fetch_data(animal_name):
-  """
-  Fetches the animals data for the animal 'animal_name'.
-  Returns: a list of animals, each animal is a dictionary:
-  {
-    'name': ...,
-    'taxonomy': {
-      ...
+    """
+    Fetches the animals data for the animal 'animal_name'.
+    Returns: a list of animals, each animal is a dictionary:
+    {
+        'name': ...,
+        'taxonomy': {
+          ...
+        },
+        'locations': [
+          ...
+        ],
+        'characteristics': {
+          ...
+        }
     },
-    'locations': [
-      ...
-    ],
-    'characteristics': {
-      ...
-    }
-  },
-  """
-  pass
+    """
+    animals_data = load_data(animal_name)
+    return listed_data(animal_name, animals_data)
