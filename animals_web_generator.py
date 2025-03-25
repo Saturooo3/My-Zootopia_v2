2e0c9a9db63b452abb4ab9cff49b33e321057247
Saturooo3 <yusufbulukgiray@gmail.com>
2025-03-25 22:34:56 +0100

    moved listed_data and serialize data from data_fetcher to animals_web_generator

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,6 +1,40 @@
 import data_fetcher
 
 
+def serialize_data(animal: dict) -> str:
+    """
+    gets a dictionary of an animal with its data and
+    returns a string with its data formatted
+    """
+    output = ""
+    output += "<li class='cards__item'>"
+
+    output += f"\n<div class='card__title'>{animal['name']}</div>"
+    output += "\n<p class ='card__text'>"
+    output += f"\n<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n"
+    output += "<strong>Locations:</strong><br/>"
+    for location in animal["locations"]:
+        output += f"\n\t{location}<br/>"
+    if "grop" in animal["characteristics"].keys():
+        output += f"\n<strong>Group</strong>: {animal["characteristics"]["group"]}<br/>\n"
+
+    output += "</p>\n</li>\n"
+    return output
+
+
+def listed_data(animal, animals_data: list) -> str:
+    """
+    gets a list of animals data and returns the animals a str with name, diet,
+    locations and type of animal for html if the list is not empty
+    """
+    output = ""
+    if len(animals_data) == 0:
+        output += f"<h2>The animal '{animal}' doesn't exist.</h2>"
+    for animal in animals_data:
+        output += serialize_data(animal)
+    return output
+
+
 def implement_animals_info(animals_str: str) -> str:
     """
     takes a str of animals data of  with name, diet, locations and type
